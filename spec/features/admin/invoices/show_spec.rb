85937f83df091502af18eaaa20baac201cca2c37
J Seymour <JustJakeSeymour@gmail.com>
2023-01-04 15:34:12 -0700

    build admin invoice show page per user story 33 spec

--- spec/features/admin/invoices/show_spec.rb
+++ spec/features/admin/invoices/show_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe "Admin Invoices Show" do
   let!(:invoice2) {customer.invoices.create!(status: 0)}
 
   describe "User Story 33" do
-    it "lists merchants in system" do
+    it "lists specific invoice info" do
       # When I visit an admin invoice show page
       visit admin_invoice_path(invoice1.id)
       # Then I see information related to that invoice including:
@@ -15,7 +15,7 @@ RSpec.describe "Admin Invoices Show" do
       # Invoice status
       expect(page).to have_content(invoice1.status)
       # Invoice created_at date in the format "Monday, July 18, 2019"
-      expect(page).to have_content(invoice1.created_at)
+      expect(page).to have_content(invoice1.created_at.strftime("%A, %B %d, %Y"))
       # Customer first and last name
       expect(page).to have_content(customer.first_name)
       expect(page).to have_content(customer.last_name)
