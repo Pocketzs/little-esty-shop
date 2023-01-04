1b701c65c9453c42a5b338238e199c9b85c7e5b7
J Seymour <JustJakeSeymour@gmail.com>
2023-01-04 14:49:19 -0700

    write spec for user story 32, Admin Invoices Index

---
+++ spec/features/admin/invoices/index_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe "Admin Invoices Index" do
+
+  describe "User Story 32" do
+    it "lists all invoice ids" do
+      # When I visit the admin Invoices index ("/admin/invoices")
+      # Then I see a list of all Invoice ids in the system
+    end
+    
+    it "each id links to admin invoice show page" do
+      # Each id links to the admin invoice show page
+    end
+  end
+  
+end
