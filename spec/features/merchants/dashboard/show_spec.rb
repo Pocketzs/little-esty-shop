b6502875244939d99eab6967ed56e7a17fe4a769
Anthony Ongaro <43623494+ajongaro@users.noreply.github.com>
2023-01-03 23:16:27 -0700

    Add test and feature for Us2, and test for Us3

--- spec/features/merchants/dashboard/show_spec.rb
+++ spec/features/merchants/dashboard/show_spec.rb
@@ -7,7 +7,20 @@ RSpec.describe 'Merchant Dashboard Index', type: :feature do
     visit "/merchants/#{merchant.id}/dashboard" 
   end
 
-  it 'displays the merchant name' do
-    expect(page).to have_content(merchant.name)
+  describe 'As a merchant, when I visit my merchant dashboard' do
+    it 'displays the merchant name' do #us1
+      expect(page).to have_content(merchant.name)
+    end
+
+    it 'shows a link to my merchant items index' do #us2
+      expect(page).to have_link("Merchant Items Index")
+      expect(page).to have_link("Merchant Invoices Index")
+    end
+
+    xit 'shows the names of the top 5 customers' do #us3
+      # who have conducted the largest number of successful transactions
+      # with my merchant and next to each customer name I see the total number of
+      # successful transactions they have conducted
+    end
   end
 end
