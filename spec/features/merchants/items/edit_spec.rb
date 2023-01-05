c892627c656948ffc87bae6432eb088519a40a67
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2023-01-04 18:29:29 -0600

    add merchant item edit spec update tests

--- spec/features/merchants/items/edit_spec.rb
+++ spec/features/merchants/items/edit_spec.rb
@@ -17,6 +17,44 @@ RSpec.describe 'The Merchant Items Show page', type: :feature do
     expect(page).to have_field('Name', with: "#{item1.name}")
     expect(page).to have_field('Description', with: "#{item1.description}")
     expect(page).to have_field('Current Selling Price', with: "#{item1.unit_price}")
-    save_and_open_page
+  end
+
+  describe 'When I update the info in the form and I click "submit"' do
+    it 'I am redirected back to the item show page where I see the updated info' do
+      visit edit_merchant_item_path(merchant1, item1)
+
+      fill_in("Name", with: "Ipad")
+      fill_in("Description", with: "Tablet for work")
+      fill_in("Current Selling Price", with: 10000)
+      click_button('Update Item')
+      expect(page).to have_current_path(merchant_item_path(merchant1, item1))
+      expect(page).to have_content("Name: Ipad")
+      expect(page).to have_content("Description: Tablet for work")
+      expect(page).to have_content("Current Selling Price: 10000")
+    end
+
+    it 'I see a flash message stating that the information has been successfully updated' do
+      visit edit_merchant_item_path(merchant1, item1)
+
+      fill_in("Name", with: "Ipad")
+      fill_in("Description", with: "Tablet for work")
+      fill_in("Current Selling Price", with: 10000)
+      click_button('Update Item')
+
+      expect(page).to have_content('Item has been updated!')
+    end
+  end
+
+  describe 'When I update the info in the form with blank fields and click submit' do
+    it 'I stay on the edit form page and I see a flash message to fill in all fields' do
+      visit edit_merchant_item_path(merchant1, item1)
+      fill_in("Name", with: "")
+      fill_in("Description", with: "")
+      fill_in("Current Selling Price", with: "")
+      click_button('Update Item')
+
+      expect(page).to have_current_path(edit_merchant_item_path(merchant1, item1))
+      expect(page).to have_content("Fields cannot be blank")
+    end
   end
 end
