c9b3b42b920a931d98bd0e6ddc63cf8009877ac7
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2023-01-04 18:30:43 -0600

    add merchant item edit link and update functionality

--- app/controllers/merchant_items_controller.rb
+++ app/controllers/merchant_items_controller.rb
@@ -6,11 +6,30 @@ class MerchantItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find_by(merchant_id: params[:merchant_id])
+    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find_by(merchant_id: params[:merchant_id])
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    # require 'pry'; binding.pry
+    item = Item.find(params[:id])
+    # binding.pry
+    if item.update(item_params)
+      redirect_to merchant_item_path(item.merchant_id, item.id)
+      flash[:alert] = 'Item has been updated!'
+    else
+      redirect_to edit_merchant_item_path(item.merchant_id, item.id)
+      flash[:alert] = "Fields cannot be blank"
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :unit_price)
   end
 end
 
