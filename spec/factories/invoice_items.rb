0930996ec4aa8083ffeeaf96cf758fc400b44125
Max MacGillivray <108768349+MoxieMax@users.noreply.github.com>
2023-01-05 13:29:41 -0700

    Revert "Pre main"

--- spec/factories/invoice_items.rb
+++ spec/factories/invoice_items.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :invoice_item do
-    item 
-    invoice 
-    quantity { rand(1..8) }
-    unit_price { [rand(99..999), rand(99..999), rand(1000..9999), rand(100000..999999)].sample }
-    status { [1..3].sample }
+    item { nil }
+    invoice { nil }
+    quantity { 1 }
+    unit_price { 1 }
+    status { 1 }
   end
 end
--- spec/factories/transactions.rb
+++ spec/factories/transactions.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :transaction do
-    invoice 
-    credit_card_number { Faker::Finance.credit_card.delete("-") }
+    invoice { nil }
+    credit_card_number { 1 }
     credit_card_expiration_date { "2023-01-03 15:02:58" }
-    result { [0, 1].sample }
+    result { 1 }
   end
 end
