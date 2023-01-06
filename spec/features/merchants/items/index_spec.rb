58f18163e2845e2c92ae3d28d02b9fd1b9d036af
J Seymour <JustJakeSeymour@gmail.com>
2023-01-05 17:10:32 -0700

    begin cleanup for pre-main merge to main

--- spec/features/merchants/items/index_spec.rb
+++ spec/features/merchants/items/index_spec.rb
@@ -1,26 +1,3 @@
-# As a merchant,
-# When I visit my merchant items index page ("merchants/merchant_id/items")
-# I see a list of the names of all of my items
-# And I do not see items for any other merchant
-
-# 7. Merchant Items Show Page
-
-# As a merchant,
-# When I click on the name of an item from the merchant items index page,
-# Then I am taken to that merchant's item's show page (/merchants/merchant_id/items/item_id)
-# And I see all of the item's attributes including:
-
-# - Name
-# - Description
-# - Current Selling Price
-
-# As a merchant
-# When I visit my items index page
-# Next to each item name I see a button to disable or enable that item.
-# When I click this button
-# Then I am redirected back to the items index
-# And I see that the items status has changed
-
 require 'rails_helper'
 
 RSpec.describe 'The Merchant Items Index page', type: :feature do
