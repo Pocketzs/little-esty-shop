83cad5bf7662d24fa6484c24f05339f620c8e4f2
J Seymour <JustJakeSeymour@gmail.com>
2023-01-07 16:55:07 -0700

    write test for user story 22, section for incomplete invoices

--- spec/features/admin/dashboard/index_spec.rb
+++ spec/features/admin/dashboard/index_spec.rb
@@ -1,33 +1,48 @@
-# 20. Admin Dashboard Links
-
-# As an admin,
-# When I visit the admin dashboard (/admin)
-# Then I see a link to the admin merchants index (/admin/merchants)
-# And I see a link to the admin invoices index (/admin/invoices)
-
 require 'rails_helper'
 
 RSpec.describe "Admin Dashboard Index Page" do
   
-  describe "when I visit the admin dashboard" do
-    it "I see a header that indicates that I am on the admin dashboard" do #us 19
+  describe "User Story 19" do
+    it "header indicating admin dashboard" do
+      # When I visit the admin dashboard (/admin)
       visit admin_dashboard_index_path
-
+      
+      # Then I see a header indicating that I am on the admin dashboard
       expect(page).to have_content("Admin Dashboard Index Page")
     end
   end
-
-  describe "when I visit the admin dashboard" do
-    it "I see a link to the admin merchants index" do #us 20
+  
+  describe "User Story 20" do
+    it "contains links" do
+      # When I visit the admin dashboard (/admin)
       visit admin_dashboard_index_path
-
+      
+      # Then I see a link to the admin merchants index (/admin/merchants)
       expect(page).to have_link("Admin Merchant Index")
-    end
-  end
-
-    it "I see a link to the admin invoices index" do
-      visit admin_dashboard_index_path
-
+      # And I see a link to the admin invoices index (/admin/invoices)
       expect(page).to have_link("Admin Invoice Index")
     end
+  end
+  
+  describe "User Story 22" do
+    it "section for incomplete invoices" do
+      # When I visit the admin dashboard
+      visit admin_dashboard_index_path
+      
+      # Then I see a section for "Incomplete Invoices"
+      within ("#incomplete_invoices") do
+        expect(page).to have_content("Incomplete Invoices")
+        # In that section I see a list of the ids of all invoices
+        # That have items that have not yet been shipped
+        expect(page).to have_content("Invoice ##{invoice1.id}")
+        expect(page).to have_content("Invoice ##{invoice2.id}")
+        expect(page).to have_content("Invoice ##{invoice3.id}")
+        
+        # And each invoice id links to that invoice's admin show page
+        expect(page).to have_link(invoice1.id)
+        click_link invoice1.id
+        expect(current_path).to eq admin_invoice_path(invoice1)
+      end
+    end
+  end
 end
