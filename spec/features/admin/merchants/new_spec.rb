8985a74d25859d8308b07ba98d70c16fd7eb0a6f
Max MacGillivray <mary.macgillivray@gmail.com>
2023-01-07 02:01:08 -0700

    Updated index view page to remedy an error, new_spec tests are passing

--- spec/features/admin/merchants/new_spec.rb
+++ spec/features/admin/merchants/new_spec.rb
@@ -34,18 +34,7 @@ RSpec.describe "Admin Merchants Create" do
       # # And I see the merchant I just created displayed
       expect(page).to have_content('Everything Topsy Turvy')
       # # And I see my merchant was created with a default status of disabled.
-      expect(page).to have_content('Disabled')
+      expect(page).to have_content('disabled')
     end
-    
-  # # As an admin,
-  # # When I visit the admin merchants index
-  # # I see a link to create a new merchant.
-  # # When I click on the link,
-  # # I am taken to a form that allows me to add merchant information.
-  # # When I fill out the form I click ‘Submit’
-  # # Then I am taken back to the admin merchants index page
-  # # And I see the merchant I just created displayed
-
-  # # And I see my merchant was created with a default status of disabled.
   end
 end
