a279d470405ffdec983dc45d9ff942d413bf9061
Lacey Weaver <jlweave89@gmail.com>
2023-01-06 21:47:19 -0700

    Add completed user story 20

--- spec/features/admin/dashboard/index_spec.rb
+++ spec/features/admin/dashboard/index_spec.rb
@@ -1,17 +1,33 @@
-# 19. Admin Dashboard
+# 20. Admin Dashboard Links
 
 # As an admin,
 # When I visit the admin dashboard (/admin)
-# Then I see a header indicating that I am on the admin dashboard
+# Then I see a link to the admin merchants index (/admin/merchants)
+# And I see a link to the admin invoices index (/admin/invoices)
+
 require 'rails_helper'
 
 RSpec.describe "Admin Dashboard Index Page" do
   
   describe "when I visit the admin dashboard" do
-    it "I see a header that indicates that I am on the admin dashboard" do
+    it "I see a header that indicates that I am on the admin dashboard" do #us 19
       visit admin_dashboard_index_path
 
       expect(page).to have_content("Admin Dashboard Index Page")
     end
   end
+
+  describe "when I visit the admin dashboard" do
+    it "I see a link to the admin merchants index" do #us 20
+      visit admin_dashboard_index_path
+
+      expect(page).to have_link("Admin Merchant Index")
+    end
+  end
+
+    it "I see a link to the admin invoices index" do
+      visit admin_dashboard_index_path
+
+      expect(page).to have_link("Admin Invoice Index")
+    end
 end
