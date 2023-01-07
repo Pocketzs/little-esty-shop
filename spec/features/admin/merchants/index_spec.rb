a610048f9deda0a9e851d11ffd3d67d2e3401cdb
Max MacGillivray <mary.macgillivray@gmail.com>
2023-01-07 00:45:10 -0700

    Started writing tests for enable/disable

--- spec/features/admin/merchants/index_spec.rb
+++ spec/features/admin/merchants/index_spec.rb
@@ -14,4 +14,29 @@ RSpec.describe "Admin Merchants Index" do
       expect(page).to have_content(merchant3.name)
     end
   end
+  
+  describe 'User Story 27' do
+    it 'has a button to enable or disable a merchant' do
+      # # When I visit the admin merchants index
+      visit admin_merchants_path
+      
+      # # Then next to each merchant name I see a button to disable or enable that merchant.
+      #within("") #within a specific section, expect page to have content, then click on it
+      expect(page).to have_button('disable') #
+      
+      # # When I click this button
+      click_on 'disable'
+      
+      # # Then I am redirected back to the admin merchants index
+      expect(current_path).to eq("/admin/merchants")
+      
+      # As an admin,
+      # When I visit the admin merchants index
+      # Then next to each merchant name I see a button to disable or enable that merchant.
+      # When I click this button
+      
+      # Then I am redirected back to the admin merchants index
+      # And I see that the merchant's status has changed
+    end
+  end
 end
