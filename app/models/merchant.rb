149bcecb07708a6798505de93f9bc6c894fc3e64
Anthony Ongaro <43623494+ajongaro@users.noreply.github.com>
2023-01-07 18:32:09 -0700

    Add enum for transaction model and tests for us12 in merchant_items index_spec

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,5 +2,12 @@ class Merchant < ApplicationRecord
   has_many :items
   has_many :invoice_items, through: :items
   has_many :invoices, through: :invoice_items
+  has_many :transactions, through: :invoices
+
   validates_presence_of :name
+
+  def top_five_items_ordered
+    binding.pry
+    self.items
+  end
 end
--- spec/features/merchants/items/index_spec.rb
+++ spec/features/merchants/items/index_spec.rb
@@ -9,6 +9,47 @@ RSpec.describe 'The Merchant Items Index page', type: :feature do
   let!(:item3) { Item.create!(name: "Pizza", description: "Cheezy Delicious", unit_price: 1749, merchant: merchant1, status: "enabled") }
   let!(:item4) { Item.create!(name: "Penguins", description: "Exotic pet", unit_price: 100005, merchant: merchant1, status: "enabled") }
   let!(:item5) { Item.create!(name: "Computer Mouse", description: "Moves Cursor", unit_price: 5000, merchant: merchant2) }
+  let!(:item6) { Item.create!(name: "Briscuit", description: "Meat", unit_price: 1000, merchant: merchant1) }
+  let!(:item7) { Item.create!(name: "Pickles", description: "Dill", unit_price: 1200, merchant: merchant1) }
+  let!(:item8) { Item.create!(name: "Headphones", description: "Plays music", unit_price: 1749, merchant: merchant1, status: "enabled") }
+  let!(:item9) { Item.create!(name: "Water Bottle", description: "Holds water", unit_price: 100005, merchant: merchant1, status: "enabled") }
+  let!(:item10) { Item.create!(name: "Laptop", description: "Writes code", unit_price: 5000, merchant: merchant2) }
+
+  let!(:customer1) {Customer.create!(first_name: "Bob", last_name: "Bobbert")}
+  let!(:customer2) {Customer.create!(first_name: "Chad", last_name: "Chaddert")}
+  let!(:customer3) {Customer.create!(first_name: "Pete", last_name: "Peterton")}
+  let!(:customer4) {Customer.create!(first_name: "Sarah", last_name: "Sarington")}
+  let!(:customer5) {Customer.create!(first_name: "Logan", last_name: "Lofferson")}
+
+  let!(:invoice1) {customer1.invoices.create!(status: 1)}
+  let!(:invoice2) {customer2.invoices.create!(status: 1)}
+  let!(:invoice3) {customer3.invoices.create!(status: 1)}
+  let!(:invoice4) {customer4.invoices.create!(status: 1)}
+  let!(:invoice5) {customer5.invoices.create!(status: 1)}
+  
+  let!(:invoice_item1) { InvoiceItem.create!(unit_price: 1000, item: item1, invoice: invoice1, status: 0) }
+  let!(:invoice_item2) { InvoiceItem.create!(unit_price: 1200, item: item2, invoice: invoice1, status: 1) }
+  let!(:invoice_item3) { InvoiceItem.create!(unit_price: 1749, item: item3, invoice: invoice2, status: 2) }
+  let!(:invoice_item4) { InvoiceItem.create!(unit_price: 100005, item: item4, invoice: invoice3, status: 1) }
+  let!(:invoice_item5) { InvoiceItem.create!(unit_price: 1000, item: item5, invoice: invoice1, status: 0) }
+  let!(:invoice_item6) { InvoiceItem.create!(unit_price: 1200, item: item6, invoice: invoice2, status: 1) }
+  let!(:invoice_item7) { InvoiceItem.create!(unit_price: 1749, item: item7, invoice: invoice3, status: 2) }
+  let!(:invoice_item8) { InvoiceItem.create!(unit_price: 100005, item: item8, invoice: invoice4, status: 1) }
+  let!(:invoice_item9) { InvoiceItem.create!(unit_price: 1000, item: item9, invoice: invoice5, status: 1) }
+  let!(:invoice_item10) { InvoiceItem.create!(unit_price: 1200, item: item2, invoice: invoice2, status: 1) }
+  let!(:invoice_item11) { InvoiceItem.create!(unit_price: 1749, item: item3, invoice: invoice3, status: 2) }
+  let!(:invoice_item12) { InvoiceItem.create!(unit_price: 100005, item: item4, invoice: invoice3, status: 1) }
+
+  let!(:transaction1) { Transaction.create!(result: "success", credit_card_number: 4832483429348594, credit_card_expiration_date: "", invoice: invoice1 ) }
+  let!(:transaction2) { Transaction.create!(result: "success", credit_card_number: 4832483429348594, credit_card_expiration_date: "", invoice: invoice2 ) }
+  let!(:transaction3) { Transaction.create!(result: "success", credit_card_number: 4832483429348594, credit_card_expiration_date: "", invoice: invoice3 ) }
+  let!(:transaction4) { Transaction.create!(result: "success", credit_card_number: 4832483429348594, credit_card_expiration_date: "", invoice: invoice4 ) }
+  let!(:transaction5) { Transaction.create!(result: "success", credit_card_number: 4832483429348594, credit_card_expiration_date: "", invoice: invoice5 ) }
+  let!(:transaction6) { Transaction.create!(result: "success", credit_card_number: 4832483429348594, credit_card_expiration_date: "", invoice: invoice1 ) }
+  let!(:transaction7) { Transaction.create!(result: "success", credit_card_number: 4832483429348594, credit_card_expiration_date: "", invoice: invoice2 ) }
+  let!(:transaction8) { Transaction.create!(result: "success", credit_card_number: 4832483429348594, credit_card_expiration_date: "", invoice: invoice3 ) }
+  let!(:transaction9) { Transaction.create!(result: "success", credit_card_number: 4832483429348594, credit_card_expiration_date: "", invoice: invoice4 ) }
+  let!(:transaction10) { Transaction.create!(result: "success", credit_card_number: 4832483429348594, credit_card_expiration_date: "", invoice: invoice5 ) }
 
   describe 'when I visit the merchant items index page' do
     it 'shows a list of the names of all my items' do
@@ -150,17 +191,22 @@ RSpec.describe 'The Merchant Items Index page', type: :feature do
   end
 
   describe "5 Most Popular Merchant Items" do #us12
+    # items belong to a merchant
+    # list of all merchants items
+    # how many invoice items for that item
+    # capture the invoices for that item
+    # calculate total sales revenue
+  
     it "shows the names of top 5 most popular items ranked by total revenue generated" do
       visit merchant_items_path(merchant1)       
-      
+
       within("#top-five-items") do
+        expect(page).to have_content(item1.name)
+        expect(page).to have_content(item2.name)
         expect(page).to have_content(item3.name)
         expect(page).to have_content(item4.name)
+        expect(page).to have_content(item5.name)
       end
-    end
-
-    xit "has an 'Disabled Items' section" do
-      visit merchant_items_path(merchant1)       
     end
   end
 end 
