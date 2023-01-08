89a04047bf47481cecd786611119d496c15c75ed
Max MacGillivray <mary.macgillivray@gmail.com>
2023-01-07 18:20:44 -0700

    Created tests for user story 28, updated test data, all tests pass, user story 28 complete

--- spec/features/admin/merchants/index_spec.rb
+++ spec/features/admin/merchants/index_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe "Admin Merchants Index" do
-  let!(:merchant1) {FactoryBot.create(:merchant)}
-  let!(:merchant2) {FactoryBot.create(:merchant)}
-  let!(:merchant3) {FactoryBot.create(:merchant)}
+  let!(:merchant1) {FactoryBot.create(:merchant, name: "Jurassic Quark")}
+  let!(:merchant2) {FactoryBot.create(:merchant, name: "A Parfait Storm")}
+  let!(:merchant3) {FactoryBot.create(:merchant, name:"Fry Hard", status: 1)}
   describe "User Story 26" do
     it "lists merchants in system" do
       # When I visit the admin merchants index (/admin/merchants)
@@ -41,6 +41,36 @@ RSpec.describe "Admin Merchants Index" do
         expect(current_path).to eq("/admin/merchants")
         expect(page).to have_content('Enabled')
         expect(page).to have_button('Disable')
+      end
+    end
+  end
+  
+  describe 'User Story 28' do
+    describe 'separates merchants by status' do
+      it "has an 'Enabled Merchants' section" do
+        # # When I visit the admin merchants index
+        visit admin_merchants_path
+        # # Then I see two sections, one for "Enabled Merchants"
+        expect(page).to have_content("Enabled Merchants")
+        # # And I see that each Merchant is listed in the appropriate section
+        within('#enabled-merchants') do
+          expect(page).to_not have_content(merchant1.name)
+          expect(page).to_not have_content(merchant2.name)
+          expect(page).to have_content(merchant3.name)
+        end
+      end
+      
+      it "has an 'Disabled Merchants' section" do
+        # # When I visit the admin merchants index
+        visit admin_merchants_path
+        # # Then I see two sections, one for "Disabled Merchants"
+        expect(page).to have_content("Disabled Merchants")
+        # # And I see that each Merchant is listed in the appropriate section
+        within('#disabled-merchants') do
+          expect(page).to have_content(merchant1.name)
+          expect(page).to have_content(merchant2.name)
+          expect(page).to_not have_content(merchant3.name)
+        end
       end
     end
   end
