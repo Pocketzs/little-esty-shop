3086159e9cbc47805d24640a6742305978e98420
J Seymour <JustJakeSeymour@gmail.com>
2023-01-10 16:50:36 -0700

    commit to switch branches

--- spec/features/admin/merchants/index_spec.rb
+++ spec/features/admin/merchants/index_spec.rb
@@ -115,12 +115,26 @@ RSpec.describe "Admin Merchants Index" do
       # When I visit the admin merchants index
       visit admin_merchants_path
     
-      # Then I see the names of the top 5 merchants by total revenue generated
-      # And I see that each merchant name links to the admin merchant show page for that merchant
-      # And I see the total revenue generated next to each merchant name
-  
+      within("#top_five_merchants") do
+        # Then I see the names of the top 5 merchants by total revenue generated
+        expect(page).to have_content(merchant2.name)
+        expect(page).to have_content(merchant3.name)
+        expect(page).to have_content(merchant4.name)
+        expect(page).to have_content(merchant5.name)
+        expect(page).to have_content(merchant6.name)
+
+        expect(merchant6.name).to appear_before(merchant5.name)
+        expect(merchant5.name).to appear_before(merchant4.name)
+        expect(merchant4.name).to appear_before(merchant3.name)
+        expect(merchant3.name).to appear_before(merchant2.name)
+
+        # And I see that each merchant name links to the admin merchant show page for that merchant
+        click_link
+        # And I see the total revenue generated next to each merchant name
+        
+        
+      end
       # Notes on Revenue Calculation:
-  
       # Only invoices with at least one successful transaction should count towards revenue
       # Revenue for an invoice should be calculated as the sum of the revenue of all invoice items
       # Revenue for an invoice item should be calculated as the invoice item unit price multiplied by the quantity (do not use the item unit price)
