e78eeb9272639bbf6ba1f81c9c683908729bbf8c
Max MacGillivray <mary.macgillivray@gmail.com>
2023-01-10 01:23:05 -0700

    Created factory for customers, updated items factory, created testing for US5

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,8 +1,11 @@
 FactoryBot.define do
   factory :item do
-    name { "MyString" }
-    description { "MyString" }
-    unit_price { 1 }
+    name {Faker::Commerce.product_name}
+    description {Faker::Coffee.notes}
+    unit_price { 1 } #Faker::Commerce.price(range: 0..10.0, as_string: true)
     merchant { nil }
   end
 end
+
+
+#GitHub page with commerce faker stuff: https://github.com/faker-ruby/faker/blob/main/doc/default/commerce.md
--- spec/features/merchants/dashboard/show_spec.rb
+++ spec/features/merchants/dashboard/show_spec.rb
@@ -3,6 +3,37 @@ require 'rails_helper'
 RSpec.describe 'Merchant Dashboard Index', type: :feature do
   let!(:merchant) { Merchant.create!(name: "Bill's Boardshop") }
 
+  let!(:item1)     {FactoryBot.create(:item, merchant_id: merchant.id)}
+  let!(:item2)     {FactoryBot.create(:item, merchant_id: merchant.id)}
+  let!(:item3)     {FactoryBot.create(:item, merchant_id: merchant.id)}
+  let!(:item4)     {FactoryBot.create(:item, merchant_id: merchant.id)}
+  let!(:item5)     {FactoryBot.create(:item, merchant_id: merchant.id)}
+  let!(:item6)     {FactoryBot.create(:item, merchant_id: merchant.id)}
+  
+  let!(:customer1) {FactoryBot.create(:customer)} 
+  let!(:customer2) {FactoryBot.create(:customer)} 
+  
+  let!(:invoice1)  {FactoryBot.create(:invoice, customer_id: customer1.id)}
+  let!(:invoice2)  {FactoryBot.create(:invoice, customer_id: customer2.id)}
+  
+  let!(:invoice_item1) {FactoryBot.create(:invoice_item, invoice_id: invoice1.id, 
+                               item_id: item1.id, quantity: 1, 
+                               unit_price: 1, status: 1)}
+  let!(:invoice_item2) {FactoryBot.create(:invoice_item, invoice_id: invoice1.id, 
+                               item_id: item2.id, quantity: 2, 
+                               unit_price: 1, status: 1)}
+  let!(:invoice_item3) {FactoryBot.create(:invoice_item, invoice_id: invoice1.id, 
+                               item_id: item3.id, quantity: 3, 
+                               unit_price: 1, status: 1)}
+  let!(:invoice_item4) {FactoryBot.create(:invoice_item, invoice_id: invoice2.id, 
+                               item_id: item4.id, quantity: 4, 
+                               unit_price: 1, status: 1)}
+  let!(:invoice_item5) {FactoryBot.create(:invoice_item, invoice_id: invoice2.id, 
+                               item_id: item5.id, quantity: 5, 
+                               unit_price: 1, status: 1)}
+  let!(:invoice_item6) {FactoryBot.create(:invoice_item, invoice_id: invoice2.id, 
+                               item_id: item6.id, quantity: 6, 
+                               unit_price: 1, status: 1)}
   before :each do
     visit "/merchants/#{merchant.id}/dashboard" 
   end
@@ -23,4 +54,24 @@ RSpec.describe 'Merchant Dashboard Index', type: :feature do
       # successful transactions they have conducted
     end
   end
+  
+  describe 'As a merchant when I visit my merchant dashboard' do
+    it "has a section for 'Items Ready to Ship'" do
+      within ('#ready_to_ship') do
+        expect(page).to have_content(merchant.name)
+      end
+    end
+    
+    it 'has the date the invoice was created by each item' do
+      # # And I see the date formatted like "Monday, July 18, 2019"
+      # # And I see that the list is ordered from oldest to newest
+    end
+  end
+  # # As a merchant
+  # # When I visit my merchant dashboard
+  # # In the section for "Items Ready to Ship",
+  # # Next to each Item name I see the date that the invoice was created
+  # # And I see the date formatted like "Monday, July 18, 2019"
+  # # And I see that the list is ordered from oldest to newest
+  
 end
