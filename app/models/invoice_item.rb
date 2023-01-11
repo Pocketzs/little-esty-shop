9a417120a687d02944b169afd1dc4d9fa35e73b2
Max MacGillivray <mary.macgillivray@gmail.com>
2023-01-11 00:48:48 -0700

    Complete User Story 5

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -3,12 +3,4 @@ class InvoiceItem < ApplicationRecord
   belongs_to :invoice
 
   enum status: ['pending', 'packaged', 'shipped']
-  
-  def invoice_date
-    invoice_ids = InvoiceItem.where("status = 0 OR status = 1").order(:created_at).pluck(:invoice_id)
-    
-    invoice_ids.map do |id|
-      Invoice.find(id).created_at
-    end
-  end
 end
