9568776e5264eebeecbce91532416bf8a26c00e2
J Seymour <110737252+JustJakeSeymour@users.noreply.github.com>
2023-01-11 11:42:35 -0700

    Update transactions.rb

--- spec/factories/transactions.rb
+++ spec/factories/transactions.rb
@@ -3,6 +3,6 @@ FactoryBot.define do
     invoice 
     credit_card_number { Faker::Number.leading_zero_number(digits: 16) }
     credit_card_expiration_date { "2023-01-03 15:02:58" }
-    result { 1 }
+    result { [0, 1].sample }
   end
 end
