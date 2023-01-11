8f7534ab376a86c9b7ddf6b2fd62d92237b6e7f3
J Seymour <110737252+JustJakeSeymour@users.noreply.github.com>
2023-01-11 11:40:06 -0700

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,4 +5,6 @@ class Item < ApplicationRecord
 
   validates_presence_of :name, :description, :unit_price
   validates_numericality_of :unit_price, only_integer: true
+  
+  enum status: [:disabled, :enabled]
 end
