2d0302affd6cef6dc7983c96810afd20d8f31d23
Anthony Ongaro <43623494+ajongaro@users.noreply.github.com>
2023-01-10 17:10:33 -0700

    Update routes to remove handrolled merchant/dashboard route

--- config/routes.rb
+++ config/routes.rb
@@ -7,13 +7,11 @@ Rails.application.routes.draw do
 
   resources :merchants, only:[] do
     resources :items, only:[:index, :show, :edit, :update], controller: "merchant_items"
-    resources :invoices, except: [:destroy], controller: "merchant_invoices"
+    resources :invoices, except:[:destroy], controller: "merchant_invoices"
+    resources :dashboard, only:[:index], controller: "merchants"
   end
 
   resources :invoices, only:[:update]
-
   resources :invoice_items, only: [:update]
-
-  get '/merchants/:id/dashboard', to: "merchants#show"
 end
 
