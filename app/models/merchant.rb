f4ed14a17d3c96ef315c3fdb99fe69ec73559461
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2023-01-10 19:14:20 -0600

    add merchant instance method items ordered functionality

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -15,4 +15,8 @@ class Merchant < ApplicationRecord
         .order("revenue desc")
         .limit(5)
   end
+
+  def items_ordered_by_most_recently_updated_at
+    self.items.order(updated_at: :desc)
+  end
 end
